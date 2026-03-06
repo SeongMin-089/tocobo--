14bcf32e41be8f93789fb8cd58559d99e7c5219a
unknown <a99701841@gmail.com>
2026-03-06 19:23:03 +0900

    update

--- src/util/footer.js
+++ src/util/footer.js
@@ -6,35 +6,35 @@ import { RiKakaoTalkFill } from "react-icons/ri";
 // 로고
 export const logoData = {
   src: img("logo.svg"),
-  alt: "TOCOBO 로고",
+  alt: "Diptyque 로고",
   href: "/",
 };
 
 // 회사 정보
 export const companyData = [
-  "주식회사 픽톤  대표 : 이병훈",
-  "주소 : 03966 서울특별시 마포구 월드컵북로15길 45 (성산동)",
-  "사업자등록번호 : 196-81-02410 [사업자정보확인]",
-  "통신판매업신고 : 제2021-서울마포-2994호 개인정보보호책임자 : 안주영",
-  "TEL : 02-6352-2410  E-MAIL : help@thepicton.com",
-  "마케팅문의 : 마케팅팀 mkt@thepicton.com",
-  "수출문의 : 해외영업팀 sales1@thepicton.com",
-  "특판&대량구매문의 : 국내영업팀 sales@thepicton.com",
+  "Diptyque Paris",
+  "Head Office: 34 Boulevard Saint-Germain, 75005 Paris",
+  "Business Registration No.: FR XX XXX XXX XXX  [Company Information]",
+  "Customer Service: +33 (0)1 43 26 77 44",
+  "E-mail: contact@diptyqueparis.com",
+  "Online Store Support: service@diptyqueparis.com",
+  "Marketing Inquiries:press@diptyqueparis.com",
+  "Corporate & Bulk Orders: corporate@diptyqueparis.com",
 ];
 export const footerLegal = {
-  copyright: "COPYRIGHT© 주식회사 픽톤 2021 TOCOBO. ALL RIGHT RESERVED.",
+  copyright: "© DIPTYQUE PARIS. ALL RIGHTS RESERVED.",
   links: [
-    { label: "이용약관", href: "/terms" },
-    { label: "개인정보처리방침", href: "/privacy" },
-    { label: "이용안내", href: "/guide" }
+    { label: "Country", href: "/terms" },
+    { label: "Language International", href: "/privacy" },
+    { label: "English", href: "/guide" }
   ]
 };
 // 고객센터
 export const customerCenterData = {
   title: "Customer Center",
-  tel: { value: "02-6352-2410", href: "tel:0263522410" },
-  hours: "AM 10:00 - PM 5:00 (Lunch 12:00 - 13:00)",
-  notice: "토, 일요일 및 공휴일 휴무",
+  tel: { value: "+1 (833) 798-0845", href: "tel:0263522410" },
+  hours: "Monday - Friday: 10 am to 6 pm EST",
+  notice: "Excluding public holidays ",
   talk: { label: "1:1 TALK", href: "#" },
 };
 
